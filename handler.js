8b58d60cf22ea7f991154e81b07b7c6caf8abd15
Nelson Gardner-Challis <nelson@tfyc.com.au>
2018-10-09 23:56:34 +1100

    working getState and saveFeedback functions

--- handler.js
+++ handler.js
@@ -37,14 +37,23 @@ module.exports.fbVerify = (event, context, callback) => {
     return callback("Invalid token");
   }
 };
+
+
+
+
 module.exports.fbMessages = (event, context, callback) => {
-  console.log(event.body);
+  console.log(JSON.stringify(event.body));
   event.body.entry.map(entry => {
     entry.messaging.map(messagingItem => {
-      // add the state handler here
+      
       console.log("Getting the state now");
-      getState(messagingItem.sender.id).then(s => {
-        if (s == "STARTED") {
+      const psid = messagingItem.sender.id;
+      console.log("This is the psid", psid);
+     
+      getState(event,messagingItem,psid).then(s => {
+        console.log("Got the state");
+        if (s == "START") {
+          console.log("Defining responsePayload for START");
           const url = `https://graph.facebook.com/v2.6/me/messages?access_token=EAACZBYlTaAHEBAMRW2CKCI0k4MCoxcjOyMkHeTzZCFQEIbWhjUefiEd5grYOMfexPbSCZAAWzSZApZCeRynWupU05ZC3d9Tpnm2dQ9keEXU7klP1xNynqyOZCVVkUYEHqSLa202BaxIra1Ddb1yNBZCcW2ah7YdHZAcx6n9862Y92sxsObm0q2Tee`;
 
           const responsePayload = {
@@ -83,51 +92,104 @@ module.exports.fbMessages = (event, context, callback) => {
               console.error("Call failed ", error);
               callback(null, error);
             });
-        } else if (s == "FOOD RATING") {
-          saveFeedback(psid, text).then(() => {
-            if (messagingItem.message.title === "No") {
-              const responsePayload = {
-                recipient: {
-                  id: messagingItem.sender.id
-                },
-                message: {
-                  text:
-                    "Well, that makes me sad. Try some food at least and give it a rating",
-                  quick_replies: [
-                    {
-                      content_type: "text",
-                      title: "Thumbs down",
-                      payload: "<POSTBACK_PAYLOAD>"
-                    },
-                    {
-                      content_type: "text",
-                      title: "Thumbs Up",
-                      payload: "<POSTBACK_PAYLOAD>"
-                    },
-                    {
-                      content_type: "text",
-                      title: "100!",
-                      payload: "<POSTBACK_PAYLOAD>"
-                    }
-                  ]
-                }
-              };
 
-              nodeFetch(url, {
-                method: "POST",
-                body: JSON.stringify(responsePayload),
-                headers: { "Content-Type": "application/json" }
-              })
-                .then(res => res.json())
-                .then(json => {
-                  console.log("Json result ", json);
-                  callback(null, json);
-                })
-                .catch(error => {
-                  console.error("Call failed ", error);
-                  callback(null, error);
-                });
-            } else if (messagingItem.message.text == "Yes") {
+            console.log("Updating the db state")
+           
+           return db
+            .update({
+              TableName: 'State',
+              Key: { psid },
+              UpdateExpression: 'set #s = :s',
+              ExpressionAttributeNames: {
+                '#s': 'state',
+              },
+              ExpressionAttributeValues: {
+                ':s': "STARTED",
+              },
+              
+            })
+            
+            .promise().then(() => saveFeedback(psid,messagingItem,s).then(() => console.log("I have saved the feedback now")))
+          }
+        else if (s == "FOOD RATING") {
+          console.log("Defining responsePayload for FOOD RATING")
+          const url = `https://graph.facebook.com/v2.6/me/messages?access_token=EAACZBYlTaAHEBAMRW2CKCI0k4MCoxcjOyMkHeTzZCFQEIbWhjUefiEd5grYOMfexPbSCZAAWzSZApZCeRynWupU05ZC3d9Tpnm2dQ9keEXU7klP1xNynqyOZCVVkUYEHqSLa202BaxIra1Ddb1yNBZCcW2ah7YdHZAcx6n9862Y92sxsObm0q2Tee`;
+          let dogs;
+          if (messagingItem.message.text =="No"){
+            dogs = "This shit sucks";
+         
+        }
+        else if (messagingItem.message.text =="Yes"){
+          dogs == "Cheers, thanks for that";
+        }
+          const responsePayload = {
+            recipient: {
+              id: messagingItem.sender.id
+            },
+            message: {
+              text:
+                dogs,
+              quick_replies: [
+                {
+                  content_type: "text",
+                  title: "Thumbs down",
+                  payload: "<POSTBACK_PAYLOAD>"
+                },
+                {
+                  content_type: "text",
+                  title: "Thumbs Up",
+                  payload: "<POSTBACK_PAYLOAD>"
+                },
+                {
+                  content_type: "text",
+                  title: "100!",
+                  payload: "<POSTBACK_PAYLOAD>"
+                }
+              ]
+            }
+          };
+
+          nodeFetch(url, {
+            method: "POST",
+            body: JSON.stringify(responsePayload),
+            headers: { "Content-Type": "application/json" }
+          })
+            .then(res => res.json())
+            .then(json => {
+              console.log("Json result ", json);
+              callback(null, json);
+            })
+            .catch(error => {
+              console.error("Call failed ", error);
+              callback(null, error);
+            });
+
+            return db
+            .update({
+              TableName: 'State',
+              Key: { psid },
+              UpdateExpression: 'set #s = :s',
+              ExpressionAttributeNames: {
+                '#s': 'state',
+              },
+              ExpressionAttributeValues: {
+                ':s': "FOOD RATING",
+              },
+              
+            })
+            
+            .promise();
+          console.log('Done.');
+        }
+      })
+    })
+  })
+}
+
+   
+            
+            
+     /*       else if (messagingItem.message.text == "Yes") {
               const url = `https://graph.facebook.com/v2.6/me/messages?access_token=EAACZBYlTaAHEBAMRW2CKCI0k4MCoxcjOyMkHeTzZCFQEIbWhjUefiEd5grYOMfexPbSCZAAWzSZApZCeRynWupU05ZC3d9Tpnm2dQ9keEXU7klP1xNynqyOZCVVkUYEHqSLa202BaxIra1Ddb1yNBZCcW2ah7YdHZAcx6n9862Y92sxsObm0q2Tee`;
 
               const responsePayload = {
@@ -171,43 +233,37 @@ module.exports.fbMessages = (event, context, callback) => {
                   callback(null, error);
                 });
             }
-          });
-        }
-      });
-    });
-  });
-};
-
+/*
 // if (messagingItem.postback.title ==="Get Started"){
 //    saveFeedback(){}.then(() =>
 //   const userKey = getPSID(event);
 //   console.log("This is the user key:", userKey);
 //might need to move const
-
-function getState(psid) {
+*/
+async function getState(event,messagingItem,psid) {
+  console.log("This is the event" , JSON.stringify(event));
   console.log(psid);
-  if (messagingItem.message.text == "Get Started") {
+  if (messagingItem.message.text == "Get Started"){
     console.log("putting STARTED into the db");
-    return db
-      .put({
+    return db.put({
         TableName: "State",
         Item: {
           psid,
-          state: "STARTED",
-          createdAt: timeStamp
+          state: "START",
+          createdAt: "cats"
         }
       })
-      .promise();
-  } else {
-    return db.getItem({
+      .promise().then(()=> "START")
+  }
+  else {
+    console.log("I need to get an item from the db")
+    return db.get({
       TableName: "State",
       Key: {
         psid: psid
       },
-      ProjectionExpression: "state"
-    });
+    }).promise().then(i =>  i.Item.state);
   }
-  return state;
 }
 //Look at the database for the state
 //If find state then return it
@@ -216,26 +272,54 @@ function getState(psid) {
 //this is from the old bot, probably not needed here at the moment
 // };
 
-function saveFeedback(psid, text) {
-  //match with user via psid
-  //take text from user input
-
+async function saveFeedback(psid, messagingItem,s) {
+const cats = messagingItem.message.text;
+var payload;
+let type;
+console.log("Working through savefeedback");
+  if (s == "START" && cats == "Get Started"){
+    console.log("Take me on a journey");
+    type = "RATING ON FOOD";
+    payload = 0;
+  }
+  else if (s == "FOOD RATING" && cats =="Thumbs Up"){
+    type = "RATING ON FOOD";
+    payload = 1;
+  }
+  else if (s == "FOOD RATING" && cats =="100!"){
+    type = "RATING ON FOOD";
+    payload = 2;
+  }
+  else if (s =="VEGAN RATING" && cats == "Nope"){
+    type = "VEGAN FEEDBACK";
+    payload = 0;
+  }
+  else if (s =="VEGAN RATING" && cats == "Yep!"){
+    type = "VEGAN FEEDBACK";
+    payload = 1;
+  }
+  else if (s =="MORE NOTIFICATIONS" && cats == "No I'm okay"){
+    type = "MORE INFO";
+    payload = 0;
+  }
+  else if (s =="MORE NOTIFICATIONS" && cats == "Obviously"){
+    type = "MORE INFO";
+    payload = 1;
+  }
   //   payload = messagingItem.message.text (?- this is probably incorrect)
   //Change it into corresponding payload for the database
   //Maybe put an if function action here for each item, or make this a generic actor
 
   //Submit to the database
-
-  return db
-    .put({
-      TableName: "Feedback",
-      Item: {
-        id: v4(),
-        psid,
-        type,
-        payload,
-        createdAt: new Date().toISOString()
-      }
+console.log("Values entering database")
+  return db.put({
+    TableName: "NewFeedback",
+    Item: {
+      psid,
+      type: type,
+      payload: payload,
+      createdAt: new Date().toISOString(),
+    }
     })
     .promise()
     .then(() => {
@@ -270,4 +354,4 @@ function saveFeedback(psid, text) {
 
           },
         };
-        */
+*/
